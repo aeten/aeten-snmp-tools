8016cfa64134a83b8c0c77c8d100ab36f3bc1afa
Thomas Pérennou <thomas.perennou@aeten.net>
2015-03-25 12:25:26 +0100

    Fix old bash compatiblity

--- src/modules-test.pl
+++ src/modules-test.pl
@@ -1,16 +1,16 @@
 #!/usr/bin/env perl
 use strict;
-use aeten::snmp::foobar::foo qw(%fooimpl);
-use aeten::snmp::foobar::bar qw(%barimpl);
+use aeten::snmp::foobar::foo qw(%foo);
+use aeten::snmp::foobar::bar qw(%bar);
 
 my $agent_name = "aeten foobar agent";
 my $index = 1;
-foreach my $hw (keys %fooimpl) {
-	my $impl=$fooimpl{$hw};
+foreach my $hw (keys %foo) {
+	my $impl=$foo{$hw};
 	$impl->update($agent_name, $index++);
 }
 $index = 1;
-foreach my $hw (keys %barimpl) {
-	my $impl=$barimpl{$hw};
+foreach my $hw (keys %bar) {
+	my $impl=$bar{$hw};
 	$impl->update($agent_name, $index++);
 }
